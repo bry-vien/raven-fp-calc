3eb40b2c8f744b0931240264d0f43253d7028fcb
Bryan Vien Bundang <bryanbundang3@gmail.com>
2026-02-27 20:57:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,40 @@
-const CACHE_NAME = 'panda-remit-v2';
-const ASSETS = [
+const CACHE_NAME = 'panda-remit-v3';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;600;700&display=swap'
+  './logo.svg'
+  // Add your CSS and JS files here, for example:
+  // './style.css',
+  // './app.js'
 ];
 
-// Install the Service Worker and cache the files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Activate and clean up old caches (This is the new part that deletes v1)
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
-
-// Fetch files from cache if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
